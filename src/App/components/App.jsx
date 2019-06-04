96088aa2b4f3f456e774aaa793d643f0b3cfbefb
Jeffrey Degoma <jhdegoma@hotmail.com>
2019-06-04 09:28:54 -0400

    recorrected duplicate guess function

--- src/App/components/App.jsx
+++ src/App/components/App.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import UserForm from './UserForm'
 import GameTable from './GameTable'
-import Fader from './Fader';
-import ReactDOM from 'react-dom';
-
-
 class App extends React.Component {
      constructor(props) {
         super(props);
         this.state = {
             text: '',
             characterName: '',
-            characters: {}
+            characters: {},
+            guessedChars:[]//real data should come from a database
         }  
         this.handleChange = this.handleChange.bind(this)
         this.handleCharacterSubmit = this.handleCharacterSubmit.bind(this)
     }
+
 
     handleChange(text) {
       this.setState({
@@ -25,38 +23,34 @@ class App extends React.Component {
 
     handleCharacterSubmit(text) {
       let arr = [].concat(this.props.chars.filter(character => character.guessed !== true))
-        this.setState({
-            characterName: text,
-            characters: arr,
-            text: ''
-        })
+      let guessedChars = [].concat(this.props.chars.filter(character => character.guessed === true))
+      this.setState({
+          characterName: text,
+          characters: arr,
+          text: '',
+          guessedChars//store correct guesses in a separate array and pass to userform
+      })
     }
 
-    // charsGuessedProp() {
-    //   //if they're all true?
-    //   let arr = [].concat(this.props.chars.filter(character => character.guessed === true))
-    //   console.log('charsGuessed?', arr)
-    // }
-   
+
     render(){
       return (
         <div className="container">
-            <h2 className="title">Can you name them all?</h2>
-            <GameTable characters={this.state.characters}
-                       charName={this.state.characterName}
-                       startingChars={this.props.chars}
-                       text={this.state.text}
-                       onTextChange={this.handleChange}
-
-            />
-            <UserForm onTextChange={this.handleChange}
+          <h2 className="title">Can you name them all???</h2>
+              <GameTable characters={this.state.characters}
+                        charName={this.state.characterName}
+                        startingChars={this.props.chars}
+                        text={this.state.text}
+                        onTextChange={this.handleChange}
+              />
+              <UserForm onTextChange={this.handleChange}
                       text={this.state.text}
                       chars={this.props.chars} 
                       handleSubmit={this.handleCharacterSubmit}
+                      alreadyGuessed={this.state.guessedChars}
              />
         </div>
       )
     }
 }
-
 export default App;
--- src/App/components/UserForm.jsx
+++ src/App/components/UserForm.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
-import Fader from './Fader';
 import swal from 'sweetalert';
-import ReactDOM from 'react-dom';
-
-
+import { isArray } from 'util';
 class UserForm extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.alreadyGuessed = this.alreadyGuessed.bind(this)
     }
-
+  
     handleChange(ev) {
         this.props.onTextChange(ev.target.value)
     }
 
+    //if user guesses the same person already
+    alreadyGuessed(guessedAgain) {
+        let guessedName = this.props.alreadyGuessed
+        guessedName.map(character => {
+             if(character.firstName === guessedAgain.toUpperCase()){
+                swal(`Whoops, ${guessedAgain.toUpperCase().trim()} was already guessed!`)
+             }
+        })
+        
+    }
     handleSubmit(ev) {
         ev.preventDefault()
-        let name = [].concat(this.props.chars.filter((character, index) => {
+        let name = this.props.chars.filter((character, index) => {
             if (character.firstName === this.props.text.toUpperCase().trim() || character.fullName === this.props.text.toUpperCase().trim()){
-               return character.guessed = !character.guessed
+               return character.guessed = true
             }
-        }))
-        if(name.length !== 0 ){
-            this.props.handleSubmit(this.props.text) 
-        }else{
+        })
+        if(name.length !== 0){
+            console.log(name)
+            this.props.handleSubmit(this.props.text)
+        }else {
             swal(`Doh! ${this.props.text.toUpperCase()} doesn\'t exist!`)
         }
+        
+        //functionality to handle duplicate guesses
+        this.alreadyGuessed(this.props.text)
     }
-
     render() {
         return (
         <form onSubmit={this.handleSubmit}>
-            <label>Type the full name, man:
-            <input type="text" onChange={this.handleChange} value={this.props.text} name="submittedName" placeholder="...El Barto"/>
+            <label>Type here:
+            <input type="text" onChange={this.handleChange} value={this.props.text} name="submittedName" placeholder="apu"/>
             </label>
             <input type="submit"/>
         </form>
         )
     }
 }
-
-//if user guesses the same person already
-
-
-
 export default UserForm;
